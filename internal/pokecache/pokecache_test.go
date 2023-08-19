d8abb567454b1e4f5ecdf93ba3f32521a8ed9a15
Abdul Rehman <ar618211@gmail.com>
2023-08-19 13:17:37 +0500

    delete outdated caches

--- internal/pokecache/pokecache_test.go
+++ internal/pokecache/pokecache_test.go
@@ -2,10 +2,11 @@ package pokecache
 
 import (
 	"testing"
+	"time"
 )
 
 func TestPokeCache(t *testing.T) {
-	cache := NewCache()
+	cache := NewCache(time.Minute)
 
 	if cache.cache == nil {
 		t.Error("cache should be a map, not nil")
@@ -40,5 +41,26 @@ func TestPokeCache(t *testing.T) {
 			t.Errorf("%v is not equal to %v", string(actual), string(c.inputVal))
 			continue
 		}
+	}
+}
+
+func TestPokeCacheReap(t *testing.T) {
+	interval := time.Millisecond * 10
+
+	cache := NewCache(interval)
+	var exists bool
+
+	cache.Add("key1", []byte("value1"))
+	_, exists = cache.Get("key1")
+
+	if !exists {
+		t.Errorf("%v should exists for now", "key1")
+	}
+
+	time.Sleep(interval)
+	_, exists = cache.Get("key1")
+
+	if exists {
+		t.Errorf("%v should not exist by now", "key1")
 	}
 }
