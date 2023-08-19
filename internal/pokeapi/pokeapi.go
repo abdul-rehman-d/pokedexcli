316da57a85e174ca3160dad701287c53b7463f08
Abdul Rehman <ar618211@gmail.com>
2023-08-19 14:02:21 +0500

    moved request func

--- internal/pokeapi/pokeapi.go
+++ internal/pokeapi/pokeapi.go
@@ -1,6 +1,8 @@
 package pokeapi
 
 import (
+	"fmt"
+	"io"
 	"net/http"
 	"time"
 
@@ -21,4 +23,44 @@ func NewClient(cacheInterval time.Duration) Client {
 		},
 		cache: pokecache.NewCache(cacheInterval),
 	}
+}
+
+func (c *Client) request(endpoint string) ([]byte, error) {
+	var data []byte
+
+	cachedRes, exists := c.cache.Get(endpoint)
+
+	if exists {
+		data = cachedRes
+		fmt.Println("cache hit")
+	} else {
+		fmt.Println("cache miss")
+		req, err := http.NewRequest("GET", endpoint, nil)
+
+		if err != nil {
+			return []byte{}, err
+		}
+
+		res, err := c.httpClient.Do(req)
+
+		if err != nil {
+			return []byte{}, err
+		}
+		defer res.Body.Close()
+
+		if res.StatusCode > 399 {
+			return []byte{}, fmt.Errorf("bad request: %v", res.StatusCode)
+		}
+
+		httpRes, err := io.ReadAll(res.Body)
+
+		if err != nil {
+			return []byte{}, err
+		}
+
+		data = httpRes
+		c.cache.Add(endpoint, data)
+	}
+
+	return data, nil
 }
--- internal/pokeapi/req_location_area.go
+++ internal/pokeapi/req_location_area.go
@@ -2,50 +2,7 @@ package pokeapi
 
 import (
 	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
 )
-
-func (c *Client) request(endpoint string) ([]byte, error) {
-	var data []byte
-
-	cachedRes, exists := c.cache.Get(endpoint)
-
-	if exists {
-		data = cachedRes
-		fmt.Println("cache hit")
-	} else {
-		fmt.Println("cache miss")
-		req, err := http.NewRequest("GET", endpoint, nil)
-
-		if err != nil {
-			return []byte{}, err
-		}
-
-		res, err := c.httpClient.Do(req)
-
-		if err != nil {
-			return []byte{}, err
-		}
-		defer res.Body.Close()
-
-		if res.StatusCode > 399 {
-			return []byte{}, fmt.Errorf("bad request: %v", res.StatusCode)
-		}
-
-		httpRes, err := io.ReadAll(res.Body)
-
-		if err != nil {
-			return []byte{}, err
-		}
-
-		data = httpRes
-		c.cache.Add(endpoint, data)
-	}
-
-	return data, nil
-}
 
 func (c *Client) ListLocationAreas(pageUrl *string) (LocationAreasResponse, error) {
 	var endpoint string
