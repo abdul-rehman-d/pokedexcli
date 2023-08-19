36c943a23c40e064dbc8120a461433cd5eba6011
Abdul Rehman <ar618211@gmail.com>
2023-08-19 05:10:58 +0500

    future proving with error

--- command_help.go
+++ command_help.go
@@ -2,11 +2,13 @@ package main
 
 import "fmt"
 
-func callbackHelp() {
+func callbackHelp() error {
 	fmt.Println("Welcome to Pokedex CLI")
 	fmt.Println("Available Commands:")
 
 	for _, cmd := range getCommands() {
 		fmt.Printf("\t%v\t\t%v\n", cmd.name, cmd.description)
 	}
+
+	return nil
 }
