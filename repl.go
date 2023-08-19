b419e9081c91dec203fd5f047e7d936b89119d7e
Abdul Rehman <ar618211@gmail.com>
2023-08-19 05:01:28 +0500

    commands and stuuf

--- repl.go
+++ repl.go
@@ -9,12 +9,62 @@ import (
 
 func startRepl() {
 	scanner := bufio.NewScanner(os.Stdin)
+
+	availableCommands := getCommands()
+
 	for {
 		fmt.Printf("> ")
 		scanner.Scan()
 
 		str := scanner.Text()
-		fmt.Printf("You said: %v\n", str)
+		cleaned := cleanInput(str)
+
+		if len(cleaned) == 0 {
+			continue
+		}
+
+		commandFromUser := cleaned[0]
+
+		command, exists := availableCommands[commandFromUser]
+
+		if !exists {
+			fmt.Println("Invalid command")
+			continue
+		}
+
+		command.callback()
+	}
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func()
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays this help menu",
+			callback:    printHelpMenu,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exits program",
+			callback: func() {
+				os.Exit(0)
+			},
+		},
+	}
+}
+
+func printHelpMenu() {
+	fmt.Println("Welcome to Pokedex CLI")
+	fmt.Println("Available Commands:")
+
+	for _, cmd := range getCommands() {
+		fmt.Printf("\t%v\t\t%v\n", cmd.name, cmd.description)
 	}
 }
 
